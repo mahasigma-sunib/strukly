013ed969779d2b24ab49e7566526bb94171daef9
Jeremy <jer.auriel@gmail.com>
2025-09-15 10:55:21 +0700

    feat: find user by email and save user methods

---
+++ strukly-backend/src/infrastructure/repositories/prisma_user_repository.ts
@@ -0,0 +1,54 @@
+// src/infrastructure/repositories/prisma_user_repository.ts
+
+import { PrismaClient } from '@prisma/client';
+import User from '../../domain/aggregates/user';
+import { UserRepository } from './user_repository';
+
+export class PrismaUserRepository implements UserRepository {
+  private prisma = new PrismaClient();
+
+  async findByEmail(email: string): Promise<User | null> {
+    const dbUser= await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!dbUser) {
+      return null;
+    }
+
+    return new User({
+      id: dbUser.id,
+      email: dbUser.email,
+      name: dbUser.name,
+      hashedPassword: dbUser.hashedPassword,
+      createdAt: dbUser.createdAt,
+      updatedAt: dbUser.updatedAt
+    })
+  }
+
+  async save(user: User): Promise<void> {
+    if (user.id) {
+      // existing user
+      await this.prisma.user.update({
+        where: { id: user.id },
+        data: {
+          email: user.email,
+          name: user.name,
+          hashedPassword: user.hashedPassword,
+          updatedAt: new Date(),
+        },
+      });
+    } else {
+      // new user
+      await this.prisma.user.create({
+        data: {
+          email: user.email,
+          name: user.name,
+          hashedPassword: user.hashedPassword,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      });
+    }
+  }
+}
