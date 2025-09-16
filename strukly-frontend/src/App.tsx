5d70d2d64e0803d6a9d0cc81918d81f5d752b716
danielson <danielsonhong@gmail.com>
2025-09-16 13:24:29 +0700

    :feat: login validation

--- strukly-frontend/src/App.tsx
+++ strukly-frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import {Routes, Route, Link} from 'react-router-dom';
-import Home from "./pages/Home"
-import TransactionHistory from "./pages/TransactionHistory"
-import TransactionDetail from './pages/TransactionDetail';
-// import UserLogin from './pages/UserLogin';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import TransactionHistory from "./pages/TransactionHistory";
+import TransactionDetail from "./pages/TransactionDetail";
+import UserLogin from "./pages/UserLogin";
 // import "./App.css";
 
-
 const App = () => {
-
   return (
     <div>
-      <nav style={{marginBottom: '1rem'}}>
-        <Link to="/">Home</Link> |{' '}
-        <Link to="/History">History</Link> |{' '}
+      <nav style={{ marginBottom: "1rem" }}>
+        <Link to="/">Home</Link> | <Link to="/History">History</Link> |{" "}
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/History" element={<TransactionHistory  />} />
-        <Route path="/History/:id" element={<TransactionDetail  />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/History" element={<TransactionHistory />} />
+        <Route path="/History/:id" element={<TransactionDetail />} />
       </Routes>
     </div>
   );
--- strukly-frontend/src/pages/UserLogin.tsx
+++ strukly-frontend/src/pages/UserLogin.tsx
@@ -1,29 +1,79 @@
 import { useState } from "react";
+import * as z from "zod";
+// import { loginSchema } from "../schemas/UserAuth";
+
+const emailSchema = z.email();
+const passwordSchema = z
+  .string()
+  .min(8, "Password must be at least 8 characters")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/\d/, "Password must contain at least one number");
 
 function UserLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // const [errors, setErrors] = useState<z.ZodIssue[]>([]);
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState<string[]>([]);
+
+  const handleEmailValidation = () => {
+    const { error, success } = emailSchema.safeParse(email);
+    if (success) {
+      setEmailError("");
+    } else {
+      setEmailError(error.issues[0].message);
+    }
+  };
+
+  const handlePasswordValidation = () => {
+    const { error, success } = passwordSchema.safeParse(password);
+    if (success) {
+      setPasswordError([]);
+    } else {
+      const errors = error.issues.map((issue) => issue.message);
+      setPasswordError(errors);
+    }
+  };
+
   return (
     <div>
-      <form>
+      <div>
         <label htmlFor="email">Email:</label>
         <input
           type="email"
           id="email"
           value={email}
-          onChange={(event) => setEmail(event?.target.value)}
+          onChange={(event) => {
+            setEmail(event?.target.value);
+            handleEmailValidation();
+          }}
           required
         />
+        {emailError != "" && <p style={{ color: "red" }}>{emailError}</p>}
+      </div>
+      <div>
         <label htmlFor="password">Password:</label>
         <input
           type="password"
           id="password"
           value={password}
-          onChange={(event) => setPassword(event?.target.value)}
+          onChange={(event) => {
+            setPassword(event?.target.value);
+            handlePasswordValidation();
+          }}
           required
         />
-        <button type="submit">Log in</button>
-      </form>
+        {passwordError.length > 0 && (
+          <div style={{ color: "red" }}>
+            {passwordError.map((error, index) => (
+              <p key={index}>{error}</p>
+            ))}
+          </div>
+        )}
+      </div>
+      <button type="submit">Log in</button>
     </div>
   );
 }
