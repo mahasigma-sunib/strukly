7059654407199bed34b8acff2865b535a1d57915
kxi-27o7 <edwardwibo270@gmail.com>
2025-09-18 19:33:06 +0700

    fixed app shrinking when informations are empty

--- strukly-frontend/src/pages/Home.tsx
+++ strukly-frontend/src/pages/Home.tsx
@@ -74,10 +74,10 @@ function Home() {
         <div style={{ width: "100%", maxWidth: "100vw", overflowX: "auto" }}>
           <div className="wallet-list">
             {Wallets.map((item) => (
-              <li key={item.id}>
+              <div className="wallet-list-card" key={item.id}>
                 <div className="item-name">{item.name}</div>
                 <div className="item-balance">{item.balance}</div>
-              </li>
+              </div>
             ))}
           </div>
         </div>
