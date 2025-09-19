ee626ae2bca51995c03b28b74de9dd65fbc6bb5c
Maltzu <edwardariatanujaya@gmail.com>
2025-09-19 14:18:28 +0700

    Memperbaiki transaction_descriptor.ts sebelum diedit tanpa sengajar

--- strukly-backend/src/domain/entities/transaction_descriptor.ts
+++ strukly-backend/src/domain/entities/transaction_descriptor.ts
@@ -1,47 +1,38 @@
-// src/domain/entities/wallet.ts
-import { randomUUID } from "crypto";
+import TransactionID from "../values/transaction_id"
 
-export type WalletProps = {
-  walletId: string;
-  userId: string;
-  walletName: string;
-  walletBalance: number;
-};
+export type TransactionDescriptorProps = {
+  id: TransactionID;
+  vendor: string;
+  categoryID: string;
+  date: Date;
+  subtotal: number;
+  tax: number;
+  total: number;
+}
 
-export default class Wallet {
-  public readonly walletId: string;
-  public readonly userId: string;
-  public walletName: string;
-  private _walletBalance: number;
+export default class TransactionDescriptor {
+  public readonly id: TransactionID;
+  public readonly vendor: string;
+  public readonly categoryID: string;
+  public readonly date: Date;
+  public readonly subtotal: number;
+  public readonly tax: number;
+  public readonly total: number;
 
-  constructor(props: WalletProps) {
-    this.walletId = props.walletId;
-    this.userId = props.userId;
-    this.walletName = props.walletName;
-    this._walletBalance = props.walletBalance;
+  constructor(props: TransactionDescriptorProps) {
+    this.id = props.id;
+    this.vendor = props.vendor;
+    this.categoryID = props.categoryID;
+    this.date = props.date;
+    this.subtotal = props.subtotal;
+    this.tax = props.tax;
+    this.total = props.total;
   }
 
-  static create(props: Omit<WalletProps, "walletId">): Wallet {
-    return new Wallet({
+    static new(props: Omit<TransactionDescriptorProps, 'id'>): TransactionDescriptor {
+    return new TransactionDescriptor({
       ...props,
-      walletId: randomUUID(),
+      id: TransactionID.fromRandom(),
     });
   }
-
-  get walletBalance(): number {
-    return this._walletBalance;
-  }
-
-  deposit(amount: number): void {
-    if (amount <= 0) throw new Error("Jumlah deposit harus lebih dari 0");
-    this._walletBalance += amount;
-  }
-
-  
-  withdraw(amount: number): void {
-    if (amount <= 0) throw new Error("Jumlah withdraw harus lebih dari 0");
-    if (amount > this._walletBalance) throw new Error("Saldo tidak mencukupi");
-    this._walletBalance -= amount;
-  }
-
 }
