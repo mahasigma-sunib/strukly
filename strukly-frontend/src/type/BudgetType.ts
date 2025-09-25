485052bb2371453816f830f437e2bb62ec9e247d
danielson <danielsonhong@gmail.com>
2025-09-25 14:32:28 +0700

    remove: removed unused type and store

--- strukly-frontend/src/type/BudgetType.ts
+++
@@ -1,9 +0,0 @@
-// export interface BudgetType {
-//   id: string; 
-//   // name: string; 
-//   // categoryId: string;
-//   amount: number; 
-//   // spentAmount: number; 
-//   period: "daily" | "weekly" | "monthly" | "yearly"; 
-//   startDate: Date;
-// }
