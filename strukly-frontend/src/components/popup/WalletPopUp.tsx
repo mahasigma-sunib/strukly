d7639a3d4bca391fb2341f4d8947534ad3096f5b
kxi-27o7 <edwardwibo270@gmail.com>
2025-09-28 20:15:18 +0700

    erased redundant functions based on prev comment

--- strukly-frontend/src/components/popup/WalletPopUp.tsx
+++ strukly-frontend/src/components/popup/WalletPopUp.tsx
@@ -33,7 +33,6 @@ const WalletPopup: React.FC<WalletPopupProps> = ({
         value={walletName}
         onChange={(e) => {
           onNameChange(e.target.value);
-          if (walletError) onNameChange(e.target.value);
         }}
       />
       <input
@@ -43,7 +42,6 @@ const WalletPopup: React.FC<WalletPopupProps> = ({
         value={walletBalance}
         onChange={(e) => {
           onBalanceChange(e.target.value);
-          if (walletError) onBalanceChange(e.target.value);
         }}
       />
       {walletError && <p className="text-red-500">{walletError}</p>}
@@ -54,40 +52,6 @@ const WalletPopup: React.FC<WalletPopupProps> = ({
         Add New Wallet
       </button>
     </Popup>
-
-    // <Popup visible={visible} onClose={onClose}>
-    //   <div className="relative flex flex-col items-center justify-center text-center gap-2.5 bg-[#242323] px-5 py-10 rounded-2xl shadow-lg">
-    //     <input
-    //       type="text"
-    //       placeholder="Wallet Name"
-    //       className="w-3/5 p-2 border border-black rounded text-base"
-    //       value={walletName}
-    //       onChange={(e) => {
-    //         onNameChange(e.target.value);
-    //         if (walletError) onNameChange(e.target.value);
-    //       }}
-    //     />
-    //     <input
-    //       type="number"
-    //       placeholder="Initial Balance"
-    //       className="w-3/5 p-2 border border-black rounded text-base"
-    //       value={walletBalance}
-    //       onChange={(e) => {
-    //         onBalanceChange(e.target.value);
-    //         if (walletError) onBalanceChange(e.target.value);
-    //       }}
-    //     />
-    //     <div>
-    //       {walletError && <p style={{ color: "red" }}>{walletError}</p>}
-    //     </div>
-    //     <button 
-    //       onClick={onAddWallet}
-    //       className="mt-4 px-5 py-2 rounded cursor-pointer bg-gray-700 text-white hover:bg-gray-800"
-    //     >
-    //       Add New Wallet
-    //     </button>
-    //   </div>
-    // </Popup>
   );
 };
 
