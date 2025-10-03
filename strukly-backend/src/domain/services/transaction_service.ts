42de07e3469ad629a82d283a35de2f238160facb
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-10-03 13:49:55 +0700

    feat(transaction): implement get methods to transaction service

--- strukly-backend/src/domain/services/transaction_service.ts
+++ strukly-backend/src/domain/services/transaction_service.ts
@@ -1,13 +1,31 @@
 import Transaction from "../aggregates/transaction";
-import TransactionHeader from "../entities/transaction_header";
 import ITransactionRepository from "../repositories/transaction_repository";
+import TransactionID from "../values/transaction_id";
+import UserID from "../values/user_id";
 
 export default class TransactionService {
-  constructor(private readonly transactionRepository: ITransactionRepository) {}
+  constructor(private readonly transactionRepository: ITransactionRepository) { }
   async createTransaction(transaction: Transaction): Promise<Transaction> {
-    return this.transactionRepository.create(transaction);
+    return await this.transactionRepository.create(transaction);
   }
   async updateTransaction(transaction: Transaction): Promise<Transaction> {
-    return this.transactionRepository.update(transaction);
+    return await this.transactionRepository.update(transaction);
+  }
+  async getTransactionListByUserID(userID: UserID): Promise<Transaction[]> {
+    return await this.transactionRepository.findByUserID(userID);
+  }
+
+  /**
+   * Get transaction by ID, ensuring it belongs to the specified user.
+   * @param userID 
+   * @param transactionID 
+   * @returns Transaction
+   */
+  async getTransactionByID(userID: UserID, transactionID: TransactionID): Promise<Transaction | null> {
+    const transaction = await this.transactionRepository.findByID(transactionID);
+    if (transaction && transaction.header.userID.equals(userID)) {
+      return transaction;
+    }
+    return null;
   }
 }
