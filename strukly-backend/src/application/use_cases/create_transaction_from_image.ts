482c38f85cbbc3e328b14e62846561bbe060cc40
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-10-03 13:12:14 +0700

    refactor(transaction): remove unused CreateTransactionFromImage use case

--- strukly-backend/src/application/use_cases/create_transaction_from_image.ts
+++
@@ -1,8 +0,0 @@
-import TransactionService from "../../domain/services/transaction_service";
-
-export default class CreateTransactionFromImage {
-  constructor(private readonly transactionService: TransactionService) { }
-  async execute(base64Image: string) {
-    this.transactionService.createTransactionFromImage(base64Image);
-  }
-}
