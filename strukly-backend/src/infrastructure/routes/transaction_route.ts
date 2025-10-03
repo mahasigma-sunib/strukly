bd5498913905bea046c69aa57aaeb2a5a07c71e5
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-10-03 13:58:59 +0700

    feat(transaction): implement get transaction list route

--- strukly-backend/src/infrastructure/routes/transaction_route.ts
+++ strukly-backend/src/infrastructure/routes/transaction_route.ts
@@ -1,29 +1,43 @@
-import { Router } from 'express';
-import TransactionController from '../controllers/transaction_controller';
-import { validateBody } from '../middleware/validation_middleware';
-import { CreateTransactionDTOSchema } from '../dto/transaction_dto';
-import { authMiddleware } from '../middleware/auth_middleware';
-import TransactionService from 'src/domain/services/transaction_service';
-import PrismaTranactionRepository from '../repositories/prisma_transaction_repository';
-import CreateTransactionUseCase from 'src/application/use_cases/transaction/create_transaction';
+import { Router } from "express";
+import TransactionController from "../controllers/transaction_controller";
+import { validateBody, validateParams } from "../middleware/validation_middleware";
+import { CreateTransactionDTOSchema } from "../dto/transaction_dto";
+import { authMiddleware } from "../middleware/auth_middleware";
+import TransactionService from "src/domain/services/transaction_service";
+import PrismaTranactionRepository from "../repositories/prisma_transaction_repository";
+import CreateTransactionUseCase from "src/application/use_cases/transaction/create_transaction";
+import GetTransactionListUseCase from "src/application/use_cases/transaction/get_transaction_list";
+import GetTransactionDetailUseCase from "src/application/use_cases/transaction/get_transaction_detail";
 
 const router = Router();
 const transactionRepository = new PrismaTranactionRepository();
 const transactionService = new TransactionService(transactionRepository);
-const createTransactionUseCase = new CreateTransactionUseCase(transactionService)
-const transactionController = new TransactionController(createTransactionUseCase);
+const createTransactionUseCase = new CreateTransactionUseCase(
+  transactionService
+);
+const getTransactionListUseCase = new GetTransactionListUseCase(
+  transactionService
+);
+const getTransactionDetailUseCase = new GetTransactionDetailUseCase(
+  transactionService
+);
+const transactionController = new TransactionController(
+  createTransactionUseCase,
+  getTransactionListUseCase,
+  getTransactionDetailUseCase
+);
 
 router.post(
-  '/transactions',
+  "/transactions",
   authMiddleware,
   validateBody(CreateTransactionDTOSchema), // Validates req.body against the schema
   transactionController.createTransaction
 );
 
-// router.get(
-//   '/transactions/:id',
-//   validateParams(z.object({ id: z.string().uuid() })),
-//   transactionController.getTransaction
-// );
+router.get(
+  "/transactions",
+  authMiddleware,
+  transactionController.getTransactionList
+);
 
 export { router as transactionRouter };
