fe3d5c447fb97a769c541b02a640db5e5fdfa6c0
jetasunanda <sunandajeta@gmail.com>
2025-11-19 22:08:05 +0700

    Add constructor in wishlist class

--- strukly-backend/src/domain/entities/wishlist.ts
+++ strukly-backend/src/domain/entities/wishlist.ts
@@ -1,20 +1,35 @@
-export class wishlist {
-    id: string;
-    itemName: string;
-    itemPrice: number;
-    progress: number;
-    userID: string;
+import WishlistID from "../values/wishlist_id";
+import UserID from "../values/user_id";
+import Money from "../values/money";
 
-    createdAt: Date;
-    updatedAt: Date;
-    
-    constructor(id: string, itemName: string, itemPrice: number, userID: string, createdAt: Date) {
-        this.id = id;
-        this.itemName = itemName;
-        this.itemPrice = itemPrice;
-        this.progress = 0;
-        this.userID = userID;
-        this.createdAt = createdAt;
-        this.updatedAt = createdAt;
-    }
+export interface IWishlistEditable {
+  itemName: string;
+  itemPrice: Money;
+  progress: number;
+}
+
+export interface IWishlistBuilder extends IWishlistEditable {
+  userID: UserID;
+}
+
+export interface IWishlistProps extends IWishlistBuilder {
+  id: WishlistID;
+}
+
+export default class Wishlist {
+  public readonly id: WishlistID;
+
+  public readonly itemName: string;
+  public readonly itemPrice: Money;
+  public readonly progress: number;
+
+  public readonly userID: UserID;
+
+  constructor(props: IWishlistProps) {
+    this.id = props.id;
+    this.itemName = props.itemName;
+    this.itemPrice = props.itemPrice;
+    this.progress = props.progress;
+    this.userID = props.userID;
+  }
 }
