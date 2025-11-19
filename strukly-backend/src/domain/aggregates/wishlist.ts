45df2cd4032d711ae71c4ca5e47e8d3927dbaab2
jetasunanda <sunandajeta@gmail.com>
2025-11-19 22:20:58 +0700

    Add new method: getWishlistID and updateWishlist

--- strukly-backend/src/domain/aggregates/wishlist.ts
+++ strukly-backend/src/domain/aggregates/wishlist.ts
@@ -2,34 +2,50 @@ import WishlistID from "../values/wishlist_id";
 import UserID from "../values/user_id";
 import Money from "../values/money";
 
-export interface IWishlistEditable {
+export default class Wishlist {
+  readonly id: WishlistID;
+
   itemName: string;
   itemPrice: Money;
   progress: number;
-}
 
-export interface IWishlistBuilder extends IWishlistEditable {
-  userID: UserID;
-}
+  readonly createdAt: Date;
+  updatedAt: Date
 
-export interface IWishlistProps extends IWishlistBuilder {
-  id: WishlistID;
-}
+  readonly userID: UserID;
 
-export default class Wishlist {
-  public readonly id: WishlistID;
-
-  public readonly itemName: string;
-  public readonly itemPrice: Money;
-  public readonly progress: number;
-
-  public readonly userID: UserID;
-
-  constructor(props: IWishlistProps) {
+  constructor(props: {
+    id: WishlistID;
+    itemName: string;
+    itemPrice: Money;
+    progress: number;
+    createdAt: Date;
+    updatedAt: Date;
+    userID: UserID;
+  }) {
     this.id = props.id;
     this.itemName = props.itemName;
     this.itemPrice = props.itemPrice;
     this.progress = props.progress;
+    this.createdAt = props.createdAt;
+    this.updatedAt = props.updatedAt;
     this.userID = props.userID;
+  }
+
+  getWishlistID(): WishlistID {
+    return this.id;
+  }
+
+  public updateWishlist(data: { itemName?: string; itemPrice?: Money; progress?: number }) {
+    if (data.itemName !== undefined) {
+      this.itemName = data.itemName;
+    }
+    if (data.itemPrice !== undefined) {
+      this.itemPrice = data.itemPrice;
+    }
+    if (data.progress !== undefined) {
+      this.progress = data.progress;
+    }
+    this.updatedAt = new Date();
   }
 }
