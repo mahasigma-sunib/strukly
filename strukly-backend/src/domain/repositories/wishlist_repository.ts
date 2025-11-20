63113cc7e81b05fb5dcd4d23cea38ed632947960
jetasunanda <sunandajeta@gmail.com>
2025-11-20 07:51:55 +0700

    Change type from "export default interface" to "export interface"

--- strukly-backend/src/domain/repositories/wishlist_repository.ts
+++ strukly-backend/src/domain/repositories/wishlist_repository.ts
@@ -2,7 +2,7 @@ import Wishlist from "../aggregates/wishlist";
 import WishlistID from "../values/wishlist_id";
 import UserID from "../values/user_id";
 
-export default interface IWishlistRepository {
+export interface IWishlistRepository {
     create: (wishlist: Wishlist) => Promise<Wishlist>;
     delete: (wishlistID: WishlistID) => Promise<void>;
     findByID: (wishlistID: WishlistID) => Promise<Wishlist | null>;
