ed17e17910c771b4deb1489380e118846a826894
jetasunanda <sunandajeta@gmail.com>
2025-11-20 13:13:27 +0700

    Make class CreateGOalItemUseCase, add constructor and async execute

--- strukly-backend/src/application/use_cases/goal_item/create_goal_item.ts
+++ strukly-backend/src/application/use_cases/goal_item/create_goal_item.ts
@@ -1,20 +1,16 @@
-import { IWishlistRepository } from "../../../domain/repositories/goal_item_repository";
+// src/application/use_cases/goal_item/create_goal_item.ts
 
+import { IGoalItemRepository } from "../../../domain/repositories/goal_item_repository";
+import GoalItem from "../../../domain/entities/goal_item";
 
-export default class CreateWishlistUseCase {
-  constructor(private readonly wishlistRepository: IWishlistRepository) { }
+export default class CreateGoalItemUseCase {
+  constructor(private readonly goalItemRepository: IGoalItemRepository) {}
 
-  async execute(
-    userID: string,
-    itemName: string,
-    itemPrice: number,
-  ) { 
-    const newWishlist = await this.wishlistRepository.create({
-      userID,
-      itemName,
-      itemPrice,
-    });
+  async execute(userID: string, name: string, price: number): Promise<GoalItem> {
+    const goal = GoalItem.new({ userID, name, price });
 
-    return newWishlist;
+    const created = await this.goalItemRepository.create(goal);
+
+    return created;
   }
 }
