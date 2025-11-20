15005bb6f344ab92c15b459d367a165960455762
jetasunanda <sunandajeta@gmail.com>
2025-11-20 22:36:45 +0700

    Add new parameter for execute (add userID) and new validation to validate the userID

--- strukly-backend/src/application/use_cases/goal_item/delete_goal_item.ts
+++ strukly-backend/src/application/use_cases/goal_item/delete_goal_item.ts
@@ -6,12 +6,17 @@ import GoalItemID from "../../../domain/values/goal_item_id";
 export default class DeleteGoalItemUseCase {
   constructor(private readonly goalItemRepository: IGoalItemRepository) {}
 
-  async execute(goalItemId: string): Promise<void> {
+  async execute(goalItemId: string, userID: string): Promise<void> {
     if (!goalItemId) throw new Error("goalItemId is required");
 
     const existing = await this.goalItemRepository.findByID(new GoalItemID(goalItemId));
     if (!existing) {
       throw new Error("GoalItem not found");
+    }
+
+    if (existing.userID.value !== userID) {
+      // Jika beda, lempar error Unauthorized/Forbidden
+      throw new Error("You are not authorized to delete this goal item");
     }
 
     await this.goalItemRepository.delete(new GoalItemID(goalItemId));
