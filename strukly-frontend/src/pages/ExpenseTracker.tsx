2824bb6e7ac7ac2baed4031b0648074eed4222e3
danielson <danielsonhong@gmail.com>
2025-11-28 20:59:27 +0700

    feat(expense): refactor ExpenseTracker to improve layout and integrate useLoadExpense for data fetching

--- strukly-frontend/src/pages/ExpenseTracker.tsx
+++ strukly-frontend/src/pages/ExpenseTracker.tsx
@@ -1,31 +1,49 @@
 import Card from "../components/card/card";
 import ExpenseList from "../components/card/ExpenseListCard";
+import useExpense, { useLoadExpense } from "../store/ExpenseStore";
 
 export default function ExpenseTracker() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+
+  useLoadExpense(month, year);
+  const { items, isLoading, error } = useExpense();
+
   return (
     <div>
-      <br />
+      <div>
+        {month}
+        {","}
+        {year}
+      </div>
 
-      <Card size="md">
-        <ExpenseList
-          vendorName="Pizza Hut"
-          date={new Date()}
-          currency="-Rp"
-          amount="85.000"
-          category="food"
-        />
-      </Card>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
 
-      <Card>
-        <ExpenseList
-          vendorName="McDonald's"
-          date={new Date()}
-          currency="-Rp"
-          amount="132.000"
-          category="food"
-        />
-      </Card>
-      
+      <div>
+        <Card size="md">
+          <ExpenseList
+            vendorName="McDonald's"
+            date={new Date()}
+            currency="-Rp"
+            amount="132.000"
+            category="food"
+          />
+        </Card>
+
+        {items.map((item) => (
+          <Card size="md">
+            <ExpenseList
+              vendorName={item.vendorName}
+              date={new Date(item.dateTime)}
+              currency={item.currency}
+              amount={item.totalAmount.toString()}
+              category={item.category}
+            />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
