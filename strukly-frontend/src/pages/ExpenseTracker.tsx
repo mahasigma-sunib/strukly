f3beb2af81c554884762b262fd81f280c68c7e36
danielson <danielsonhong@gmail.com>
2025-11-28 21:05:13 +0700

    feat(expense): enhance ExpenseTracker to display month name instead of month number

--- strukly-frontend/src/pages/ExpenseTracker.tsx
+++ strukly-frontend/src/pages/ExpenseTracker.tsx
@@ -7,13 +7,30 @@ export default function ExpenseTracker() {
   const month = today.getMonth() + 1;
   const year = today.getFullYear();
 
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const monthName = monthNames[month - 1];
+
+
   useLoadExpense(month, year);
   const { items, isLoading, error } = useExpense();
 
   return (
     <div>
-      <div>
-        {month}
+      <div className="font-bold text-3xl m-5">
+        {monthName}
         {","}
         {year}
       </div>
