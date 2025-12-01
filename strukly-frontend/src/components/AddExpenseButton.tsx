5f712598d8cbc2183a726fa06b4caea2145d344c
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-12-01 16:52:09 +0700

    fix(AddExpenseButton): add reflection effect to enhance button interaction

--- strukly-frontend/src/components/AddExpenseButton.tsx
+++ strukly-frontend/src/components/AddExpenseButton.tsx
@@ -5,7 +5,8 @@ export default function AddExpenseButton() {
   return (
     <Link to="/add">
       <div className="-mt-5 w-18 h-17 bg-white rounded-full flex justify-center items-center">
-        <div className="flex items-center justify-center w-12 h-12 bg-[#FFC606] border-6 border-[#FFE432] rounded-full shadow-[0_5px_0_0_#FFAA28] active:shadow-none active:translate-y-1 transition-all duration-100">
+        <div className="group relative flex items-center justify-center w-12 h-12 bg-[#FFC606] border-6 border-[#FFE432] rounded-full shadow-[0_5px_0_0_#FFAA28] active:shadow-none active:translate-y-1 transition-all duration-100 overflow-hidden">
+          <div className="absolute inset-0 bg-linear-to-r from-transparent via-white/50 to-transparent -translate-x-full group-active:translate-x-full transition-transform duration-300 ease-in-out skew-x-12" />
           <AddIcon width={22} height={22} />
         </div>
       </div>
