00b1eadc6e96f3ccc7e928ca1ce8bda48b9517ef
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-12-03 11:05:08 +0700

    fix(MobileNavBar): remove conflicting Link and onClick for add expense button

--- strukly-frontend/src/components/MobileNavBar.tsx
+++ strukly-frontend/src/components/MobileNavBar.tsx
@@ -73,13 +73,13 @@ export default function MobileNavBar() {
       />
 
       <div className="flex justify-center">
-        <Link to="/newExpense" onClick={() => setIsDrawerOpen(true)}>
+        <button onClick={() => setIsDrawerOpen(true)}>
           <div className="-mt-5 w-18 h-17 bg-white rounded-full flex justify-center items-center">
             <div className="flex items-center justify-center w-12 h-12 bg-[#FFC606] border-6 border-[#FFE432] rounded-full shadow-[0_5px_0_0_#FFAA28] active:shadow-none active:translate-y-1 transition-all duration-100">
               <AddIcon width={22} height={22} />
             </div>
           </div>
-        </Link>
+        </button>
       </div>
 
       <Drawer
