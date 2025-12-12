76b0503aa723a18a1f87f14f06784883f1cecf36
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-12-12 13:35:24 +0700

    feat: add PWA support with icons and manifest configuration

--- strukly-frontend/vite.config.ts
+++ strukly-frontend/vite.config.ts
@@ -1,8 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Koinku",
+      short_name: "Koinku",
+      description: "Your expense tracker and budget buddy.",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ],
+      start_url: "/",
+      display: "standalone",
+    }
+  })],
 });
