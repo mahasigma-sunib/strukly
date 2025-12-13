a1da2c8b5fdb46bba35b32518644b1cea128817f
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-12-13 11:32:19 +0700

    feat: pwa screenshot

--- strukly-frontend/vite.config.ts
+++ strukly-frontend/vite.config.ts
@@ -3,42 +3,53 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: "autoUpdate",
-    manifest: {
-      name: "Koinku",
-      short_name: "Koinku",
-      description: "Your expense tracker and budget buddy.",
-      theme_color: "#ffffff",
-      icons: [
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable'
-        }
-      ],
-      start_url: "/",
-      display: "standalone",
-    }
-  })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Koinku",
+        short_name: "Koinku",
+        description: "Your expense tracker and budget buddy.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        screenshots: [
+          {
+            src: "screenshot1080x1920.png",
+            sizes: "1080x1920",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+      },
+    }),
+  ],
 });
