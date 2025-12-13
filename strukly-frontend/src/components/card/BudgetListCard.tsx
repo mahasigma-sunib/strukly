03d726350bc664dcf02536cadf7298f98410bffa
danielson <danielsonhong@gmail.com>
2025-12-13 21:19:11 +0700

    refactor: streamline category icon handling in BudgetListCard component

--- strukly-frontend/src/components/card/BudgetListCard.tsx
+++ strukly-frontend/src/components/card/BudgetListCard.tsx
@@ -1,39 +1,12 @@
-import FoodIcon from "../categoryIcons/FoodIcon";
-import GroceriesIcon from "../categoryIcons/GroceriesIcon";
-import VehicleIcon from "../categoryIcons/VehicleIcon";
-import ShoppingIcon from "../categoryIcons/ShoppingIcon";
-import EntertainmentIcon from "../categoryIcons/EntertainmentIcon";
-import OthersIcon from "../categoryIcons/OthersIcon";
-import UtilityIcon from "../categoryIcons/UtilityIcons";
+import { getCategoryData } from "../../utils/CategoryConfig";
 import ProgressBar from "../graph/ProgressBar";
-import type React from "react";
 
 interface BudgetListProps {
   currency: string;
   spent: number;
   budget: number;
-  category: keyof typeof categoryColors;
+  category: string;
 }
-
-const categoryColors: Record<string, string> = {
-  Food: "var(--fun-color-category-food)",
-  Groceries: "var(--fun-color-category-groceries)",
-  Transportation: "var(--fun-color-category-transportation)",
-  Utilities: "var(--fun-color-category-housebills)",
-  Shopping: "var(--fun-color-category-shopping)",
-  Entertainment: "var(--fun-color-category-entertainment)",
-  Others: "var(--fun-color-category-others)",
-};
-
-const categoryIconMap: Record<keyof typeof categoryColors, React.ReactNode> = {
-  Food: <FoodIcon />,
-  Groceries: <GroceriesIcon />,
-  Transportation: <VehicleIcon />,
-  Shopping: <ShoppingIcon />,
-  Entertainment: <EntertainmentIcon />,
-  Utilities: <UtilityIcon />,
-  Others: <OthersIcon />,
-};
 
 export default function BudgetList({
   currency,
@@ -41,11 +14,14 @@ export default function BudgetList({
   budget,
   category,
 }: BudgetListProps) {
+  const { icon } = getCategoryData(category);
 
   const spentNum = spent;
   const budgetNum = budget;
 
-  const idFormatter = new Intl.NumberFormat("id-ID", { maximumFractionDigits: 0 });
+  const idFormatter = new Intl.NumberFormat("id-ID", {
+    maximumFractionDigits: 0,
+  });
   const spentString = `${idFormatter.format(spentNum)}`;
   const budgetString = `${idFormatter.format(budgetNum)}`;
 
@@ -55,7 +31,7 @@ export default function BudgetList({
         {/* left */}
         <div className="flex gap-4 items-center w-1/4">
           {/* icon */}
-          <div className="">{categoryIconMap[category]}</div>
+          <div className="">{icon}</div>
         </div>
 
         {/* right */}
