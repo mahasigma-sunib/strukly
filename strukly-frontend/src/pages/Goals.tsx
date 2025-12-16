17265d535cac7142eea7714ff8ad2627afde39e7
AyamBerenang <varel.lie29@gmail.com>
2025-12-16 20:21:49 +0700

    not really important

--- strukly-frontend/src/pages/Goals.tsx
+++ strukly-frontend/src/pages/Goals.tsx
@@ -1,3 +1,13 @@
-export default function Goals(){
-    return <div></div>
+import FlagIcon from "../components/utilityIcons/FlagIcon";
+
+export default function Goals() {
+  return (
+    <div className="m-4 my-7 flex items-center justify-between">
+      <div className="flex flex-col gap-2">
+        <p className="font-bold text-3xl">Goals</p>
+        <p className="text-lg">One Day or Day One!</p>
+      </div>
+      <div className=""></div>
+    </div>
+  );
 }
--- strukly-frontend/src/pages/Home.tsx
+++ strukly-frontend/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import TextLogo from "../components/logos/TextLogo";
+import HappyMascot from "../components/mascots/HappyMascot";
 import useUserAuth from "../store/UserAuthStore";
 // import "../css/WalletPopUp.css";
 
@@ -5,9 +7,18 @@ function Home() {
   const username = useUserAuth((s) => s.user?.name || "User");
 
   return (
-    <div className="m-8 font-bold text-2xl">
-      <h1>Hi, {username}</h1>
-    </div>
+    <>
+      <div className="mt-4 ml-3 mb-2">
+        <TextLogo width={100} height={40} />
+      </div>
+
+      <div className="flex flex-row gap-4 p-4 items-center">
+        <div className="flex flex-col gap-1">
+          <p className="text-2xl font-semibold text-text-secondary">Hello,</p>
+          <p className="text-3xl font-extrabold">{username}</p>
+        </div>
+      </div>
+    </>
   );
 }
 
