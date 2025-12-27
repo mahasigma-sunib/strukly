910f27acc83396fab7b742768d351701e11a71c7
danielson <danielsonhong@gmail.com>
2025-12-27 20:08:48 +0700

    feat: add DropdownIcon component for utility icons

---
+++ strukly-frontend/src/components/utilityIcons/DropdownIcon.tsx
@@ -0,0 +1,21 @@
+import { type SVGProps } from "react";
+
+interface DropDownIconProps extends SVGProps<SVGSVGElement> {}
+
+export default function DropDownIcon(props: DropDownIconProps) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M5 8.00001L12 16L19 8"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
