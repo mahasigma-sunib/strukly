fd47817523953609c8d52d702dd691539a99857c
AyamBerenang <varel.lie29@gmail.com>
2025-12-28 17:35:32 +0700

    feat: enhance Home page UI with new mascots, budget display, and recent expenses section

--- strukly-frontend/src/pages/Home.tsx
+++ strukly-frontend/src/pages/Home.tsx
@@ -4,8 +4,11 @@ import SettingsIcon from "../components/utilityIcons/SettingsIcon";
 import Button from "../components/button/Button";
 import HappyMascot from "../components/mascots/HappyMascot";
 import HeadbandMascot from "../components/mascots/HeadbandMascot";
-import NeutralMascot from "../components/mascots/NeutralMascot";
+import PeekingUpMascot from "../components/mascots/PeekingUpMascot";
+import Card from "../components/card/Card";
 import WhistleMascot from "../components/mascots/WhistleMascot";
+import WinkMascot from "../components/mascots/WinkMascot";
+import NeutralMascot from "../components/mascots/NeutralMascot";
 
 const getGreeting = () => {
   const hour = new Date().getHours();
@@ -48,49 +51,111 @@ function Home() {
 
   return (
     <div>
-      <div className="p-4">
-        <TextLogo width={96} />
+      <div className="px-4 py-2 bg-surface z-999 fixed w-full border-b-1 border-border">
+        <TextLogo width={80} />
       </div>
-      <div className="px-5 flex flex-col gap-6">
-        <div className="flex flex-row justify-between items-center">
+
+      <div className="flex flex-col bg-surface py-4 border-b-2 border-border rounded-b-3xl mb-3">
+        <div className="px-5 flex flex-row justify-between items-center pt-12">
           <div className="flex flex-row gap-4 items-center">
             <div>
-              <HappyMascot width={48} height={48} />
+              <WinkMascot width={48} height={48} />
             </div>
-            <div>
-              <p className="font-semibold text-lg text-light-gray">
+            <div className="flex flex-col">
+              <p className="font-semibold text-lg text-text-disabled/70">
                 Good {greeting},
               </p>
               <p className="font-bold text-2xl">{username}</p>
             </div>
           </div>
           <div>
-            <Button
-              variant="secondary"
-              size="sm"
-              className="!p-2 rounded-2xl items-center"
-            >
+            <Button variant="blue" size="sm" className="!p-1 rounded-2xl">
               <SettingsIcon width={28} />
             </Button>
           </div>
         </div>
-        <div className="p-4 bg-orange text-white rounded-3xl">
-          <div className="flex flex-row justify-between mb-4">
-            <p className="font-bold text-xl ">Dashboard</p>
-            <p className="px-3 py-1 bg-surface font-extrabold text-orange rounded-full">
-              {currentMonthYear}
-            </p>
+      </div>
+
+      <div className="relative flex flex-col p-5">
+        <div className="w-fit mb-4">
+          <p className="text-2xl font-bold text-text-primary">
+            🗓️ {currentMonthYear}
+          </p>
+        </div>
+
+        <div className="absolute right-10 top-1 z-0">
+          <div className=" rounded-full flex items-center justify-center text-xs font-bold ">
+            <HappyMascot width={100} height={100} />
           </div>
-          <div className="flex flex-col items-center justify-center gap-1">
-            <p className="text-lg">Total Expense</p>
-            <p className="font-bold text-3xl tracking-wide">
+        </div>
+
+        <div className="relative p-4 bg-gradient-to-br from-[#ff8717] to-[#fabd15] rounded-2xl flex flex-col shadow-[0_6px_0_0_#e9a613] z-10 ">
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <p className="font-bold text-lg text-white/80">You've spent</p>
+            <p className="font-bold text-4xl tracking-wide text-white">
               Rp2.540.000{/* total expense goes here */}
               <span className="font-semibold text-xl text-white/80">,00</span>
             </p>
           </div>
+          <div className="mt-4 p-4 bg-surface flex flex-col gap-2 rounded-xl">
+            <p className="text-text-primary/80 font-bold text-2xl">Budget</p>
+            <div>
+              <p className="font-bold text-inactive">
+                Budget Progress Bar goes here!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-          <div className="bg-surface p-4 rounded-2xl text-text-primary mt-4">
-            <p>Budget</p>
+      <div className="px-5">
+        <Card className="!m-0">
+          <div className="flex flex-row justify-between items-center gap-3 ">
+            <p className="text-[40px] text-center">💸</p>
+            <p className="text-lg text-text-secondary">
+              Per day, you spend on average{" "}
+              <span className="text-xl font-bold text-text-primary">
+                Rp200.000
+              </span>{" "}
+            </p>
+          </div>
+        </Card>
+      </div>
+
+      <div className="pb-24">
+        <div className="p-5 mt-4 flex flex-col gap-8">
+          <div>
+            <p className="text-2xl font-bold mb-2">Current Goals</p>
+            {/* Goals goes here */}
+            <Card className="!m-0">
+              <div className="p-5 items-center justify-center flex flex-col gap-4 bg-surface rounded-2xl">
+                <HeadbandMascot width={72} height={72} />
+                <p className="text-inactive font-semibold text-base text-center">
+                  You haven't made a goal yet
+                </p>
+                {/* Link to Goals Page */}
+                <Button size="lg" variant="primary" className="!py-2">
+                  Make a goal
+                </Button>
+              </div>
+            </Card>
+          </div>
+
+          <div>
+            <p className="text-2xl font-bold mb-2">Recent Expenses</p>
+            {/* Recent expenses goes here */}
+            <Card className="!m-0">
+              <div className="p-5 items-center justify-center flex flex-col gap-4 bg-surface rounded-2xl">
+                <WhistleMascot width={72} height={72} />
+                <p className="text-inactive font-semibold text-base text-center">
+                  You haven't add a new expense yet
+                </p>
+                {/* Link to Goals Page */}
+                <Button size="lg" variant="primary" className="!py-2">
+                  Add expense
+                </Button>
+              </div>
+            </Card>
           </div>
         </div>
       </div>
