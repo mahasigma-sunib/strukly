b083132cc78be45743827ea01d9432ec6ec9849f
jetasunanda <sunandajeta@gmail.com>
2025-12-31 15:19:50 +0700

    fix: remove commented-out code in GoalsHeader for cleaner readability

--- strukly-frontend/src/components/GoalsHeader.tsx
+++ strukly-frontend/src/components/GoalsHeader.tsx
@@ -12,13 +12,9 @@ const GoalsHeader: React.FC<Props> = ({ onAdd }) => {
     <div className="sticky top-0 z-20 flex items-center justify-between mb-8 p-5 rounded-b-3xl border-b-2 border-border bg-surface">
       <div>
         <h1 className="text-3xl font-bold ">Goals</h1>
-        {/* <p className="text-sm font-bold text-active bg-secondary-hover/10 px-2 py-1 rounded-md inline-block mt-1">
-            {activeCount} Active Targets
-          </p> */}
       </div>
       <Button
         onClick={onAdd}
-        // className="bg-[var(--fun-color-secondary)] p-3 rounded-2xl hover:scale-105 transition-transform flex items-center justify-center"
         className="rounded-2xl !p-3"
         variant="secondary"
         size="md"
